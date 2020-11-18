6cc3c3b5754f6f158289a71f24086b0329687db7
MKSampei <marcelo.sampei@gmail.com>
2020-11-18 16:58:03 -0300

    atualizaçao jsonignore

--- src/main/java/com/samarici/cursomc/domain/Pagamento.java
+++ src/main/java/com/samarici/cursomc/domain/Pagamento.java
@@ -10,7 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.samarici.cursomc.domain.enums.EstadoPagamento;
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
@@ -23,7 +23,7 @@ public abstract class Pagamento implements Serializable {
 	@Id
 	private Integer id;
 	private Integer estadoPagto;
-	@JsonBackReference
+	@JsonIgnore
 	@OneToOne
 	@JoinColumn(name = "pedido_id")
 	@MapsId

705821cfdd62ebaed68c5a59f95360efd8814279
MKSampei <marcelo.sampei@gmail.com>
2020-11-18 22:03:36 -0300

    Listando todas as Categorias com DTO

--- src/main/java/com/samarici/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/samarici/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.samarici.cursomc.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.samarici.cursomc.domain.Categoria;
+import com.samarici.cursomc.dto.CategoriaDTO;
 import com.samarici.cursomc.services.CategoriaService;
 
 @RestController
@@ -47,5 +50,13 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+
+		List<Categoria> lista = service.findAll();
+		
+		List<CategoriaDTO> listaDto = lista.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listaDto);
+	}
 }
--- src/main/java/com/samarici/cursomc/services/CategoriaService.java
+++ src/main/java/com/samarici/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.samarici.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,10 @@ public class CategoriaService {
 		}catch(DataIntegrityViolationException e) {
 			throw new com.samarici.cursomc.services.exception.DataIntegrityException("Não é possivel excluir uma categoria que tem produtos.");
 		}
+	}
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 
 }
